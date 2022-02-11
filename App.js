38d116fc207e2ccfff3c49a1340f4d7f9d5071c5
sypark96 <sypark96@nkia.co.kr>
2022-02-11 14:17:16 +0900

    todo 수정 및 완료 체크박스 추가

--- App.js
+++ App.js
@@ -8,31 +8,46 @@ import {
   TextInput,
   ScrollView,
   Alert,
+  ActivityIndicator,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { FontAwesome } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { theme } from "./colors";
+// import { EditModal } from "./EditModal";
 
 const STORAGE_KEY = "@toDos";
+const IS_WORKING = "@working";
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
+  const [editText, setEditText] = useState("");
   const [toDos, setToDos] = useState({});
+  const [editItem, setEditItem] = useState(null);
 
   useEffect(() => {
-    console.log("useEffect");
     loadToDos();
   }, []);
 
-  const travel = () => setWorking(false);
-
-  const work = () => setWorking(true);
+  const _setWorking = async (work) => {
+    try {
+      setEditItem(null);
+      await AsyncStorage.setItem(IS_WORKING, JSON.stringify(work));
+      setWorking(work);
+    } catch (e) {
+      console.log("_setWorking error! ==> ", e);
+    }
+  };
 
   const onChangeText = (payload) => setText(payload);
 
+  const onChangeTodoText = (payload) => setEditText(payload);
+
   const saveToDos = async (toSave) => {
     try {
+      setEditItem(null);
       await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
     } catch (e) {
       console.log("saveToDos error! ==> ", e);
@@ -42,7 +57,9 @@ export default function App() {
   const loadToDos = async () => {
     try {
       const s = await AsyncStorage.getItem(STORAGE_KEY);
+      const isWorking = await AsyncStorage.getItem(IS_WORKING);
       setToDos(s !== null ? JSON.parse(s) : []);
+      setWorking(isWorking !== null ? JSON.parse(isWorking) : true);
     } catch (e) {
       console.log("loadToDos error! ==> ", e);
     }
@@ -56,7 +73,10 @@ export default function App() {
       // const newToDos = Object.assign({}, toDos, {
       //   [Date.now()]: { text, work: working },
       // });
-      const newToDos = { ...toDos, [Date.now()]: { text, working } };
+      const newToDos = {
+        ...toDos,
+        [Date.now()]: { text, working, completed: false },
+      };
       setToDos(newToDos);
       setText("");
       await saveToDos(newToDos);
@@ -86,18 +106,50 @@ export default function App() {
     }
   };
 
+  const updateTodoState = async (id, state) => {
+    try {
+      const newTodos = { ...toDos };
+      newTodos[id].completed = !state;
+      setToDos(newTodos);
+      await saveToDos(newTodos);
+    } catch (e) {
+      console.log("updateTodoState error! ==> ", e);
+    }
+  };
+
+  const updateTodoText = async (id) => {
+    try {
+      if (editText !== "") {
+        const newToDos = { ...toDos };
+        newToDos[id].text = editText;
+        setToDos(newToDos);
+        await saveToDos(newToDos);
+      }
+    } catch (e) {
+      console.log("updateTodoText error! ==> ", e);
+    }
+  };
+
+  const editTodo = async (id) => {
+    if (editItem !== id) {
+      setEditItem(id);
+    } else {
+      updateTodoText(id);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
       <View style={styles.header}>
-        <TouchableOpacity onPress={work}>
+        <TouchableOpacity onPress={() => _setWorking(true)}>
           <Text
             style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
           >
             Work
           </Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={travel}>
+        <TouchableOpacity onPress={() => _setWorking(false)}>
           <Text
             style={{
               ...styles.btnText,
@@ -119,18 +171,80 @@ export default function App() {
         />
       </View>
       <ScrollView>
-        {Object.keys(toDos).map((key) =>
-          toDos[key].working === working ? (
-            <View key={key} style={styles.toDo}>
-              <Text style={styles.toDoText}>{toDos[key].text}</Text>
-              <TouchableOpacity onPress={() => deleteToDo(key)}>
-                {/* <Text>❌</Text> */}
-                <FontAwesome name="trash-o" size={15} color="white" />
-              </TouchableOpacity>
-            </View>
-          ) : null
+        {Object.keys(toDos).length > 0 ? (
+          Object.keys(toDos).map((key) =>
+            toDos[key].working === working ? (
+              <View key={key} style={styles.toDo}>
+                {editItem && editItem === key ? (
+                  <TextInput
+                    style={styles.editInput}
+                    onChangeText={onChangeTodoText}
+                    onSubmitEditing={() => updateTodoText(key)}
+                    returnKeyType="done"
+                    defaultValue={toDos[key].text}
+                  />
+                ) : (
+                  <TouchableOpacity
+                    onPress={() => updateTodoState(key, toDos[key].completed)}
+                  >
+                    <View style={styles.checkableText}>
+                      {toDos[key].completed ? (
+                        <MaterialCommunityIcons
+                          name="checkbox-marked-outline"
+                          size={15}
+                          color={theme.completedText}
+                        />
+                      ) : (
+                        <MaterialCommunityIcons
+                          name="checkbox-blank-outline"
+                          size={15}
+                          color="white"
+                        />
+                      )}
+                      <Text
+                        style={{
+                          ...styles.toDoText,
+                          textDecorationLine: toDos[key].completed
+                            ? "line-through"
+                            : "none",
+                          color: toDos[key].completed
+                            ? theme.completedText
+                            : "white",
+                        }}
+                      >
+                        {toDos[key].text}
+                      </Text>
+                    </View>
+                  </TouchableOpacity>
+                )}
+
+                <View style={{ flexDirection: "row" }}>
+                  <TouchableOpacity
+                    onPress={() => editTodo(key)}
+                    style={{ marginRight: 15 }}
+                  >
+                    {editItem !== null && editItem === key ? (
+                      <Feather name="edit-3" size={15} color="white" />
+                    ) : (
+                      <Feather name="edit-2" size={15} color="white" />
+                    )}
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => deleteToDo(key)}>
+                    <FontAwesome name="trash-o" size={15} color="white" />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            ) : null
+          )
+        ) : (
+          <ActivityIndicator
+            color="white"
+            size="large"
+            style={{ marginTop: 50 }}
+          />
         )}
       </ScrollView>
+      {/* <EditModal visible={visible} /> */}
     </View>
   );
 }
@@ -158,6 +272,13 @@ const styles = StyleSheet.create({
     marginVertical: 20,
     fontSize: 15,
   },
+  editInput: {
+    backgroundColor: "white",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    width: 250,
+  },
   toDo: {
     backgroundColor: theme.grey,
     marginBottom: 10,
@@ -172,5 +293,10 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 15,
     fontWeight: "500",
+    marginLeft: 10,
+  },
+  checkableText: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
--- colors.js
+++ colors.js
@@ -2,4 +2,5 @@ export const theme = {
   bg: "black",
   grey: "#3a3d40",
   toDoBg: "#5c5c60",
+  completedText: "#a3a8ad",
 };
